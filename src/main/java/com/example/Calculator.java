e8b152e6411ea1bbf23cab802bea4a9ff8945046
Advika116 <advikajahagirdar2024.it@mmcoe.edu.in>
2026-09-24 09:40:33 +0530

    Update calculator output

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -23,6 +23,6 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Addition Result: " + calc.add(10, 5));
     }
 }
